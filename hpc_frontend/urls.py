e5eeb6b0bdbc0b4f007a35de5ebccfcda45f240f
William Zhang <willzhang05@gmail.com>
2019-04-24 20:28:21 +0000

    add initial auth stuff

--- hpc_frontend/urls.py
+++ hpc_frontend/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view()),
+    path('logout/', auth_views.LogoutView.as_view()),
     path('', include('landing.urls')),
 ]
